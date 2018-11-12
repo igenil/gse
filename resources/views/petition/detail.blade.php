837d83bf7883586e913206d3ddc17d129bbb6605
JavierPartidaMolina <38098306+JavierPartidaMolina@users.noreply.github.com>
2018-11-12 19:07:56 +0100

    rutas petitions

--- resources/views/petition/detail.blade.php
+++ resources/views/petition/detail.blade.php
@@ -22,11 +22,11 @@
                 <td>{{$petition->n_Students}}</td>
                 <td>{{$petition->created_at}}</td>
                 <td style="width:1%;">
-                <a href="editgrade/{{$grade->id}}">
+                <a href="editpetition/{{$petition->id}}">
                     <button class = "btn btn-primary"><span class="fas fa-pencil-alt" ></button></td>
                 </a>
                 <td style="width:1%;">
-                <form action="grade/{{ $grade->id}}" method="post">
+                <form action="petition/{{ $petition->id}}" method="post">
                 {{ method_field('DELETE') }}
                 {{ csrf_field() }}
                     <button class="btn btn-danger" type="submit"><span class="fas fa-trash-alt" ></button>
@@ -36,8 +36,8 @@
             </tr>
             @endforeach   
             <td>
-                <a href="{{ url('/addgrade') }}">  
-                    <button type="submit" class="btn btn-success">Add Grade</button>
+                <a href="{{ url('/addpetition') }}">  
+                    <button type="submit" class="btn btn-success">Add Petition</button>
                 </a>
             </td>
             <td></td>
--- routes/web.php
+++ routes/web.php
@@ -24,18 +24,23 @@ Route::get('addgrade', 'GradeController@indexaddgrade');
 Route::get('addstudent', 'StudentController@indexaddstudent');
 Route::get('student', 'StudentController@index');
 Route::get('petition', 'PetitionController@index');
+Route::get('addpetition', 'PetitionController@indexaddpetition');
+Route::get('addpetition/{id}', 'PetitionController@indexeditpetition');
 Route::get('editgrade/{id}', 'GradeController@indexeditgrade');
 Route::get('editcompany/{id}', 'CompanyController@indexeditcompany');
 Route::get('editstudent/{id}', 'StudentController@indexeditstudent');
 
+Route::post('addpetition', 'PetitionController@store');
 Route::post('addcompany', 'CompanyController@store');
 Route::post('addgrade', 'GradeController@store');
 Route::post('addstudent', 'StudentController@store');
 
+Route::delete('petition/{id}', 'PetitionController@destroy');
 Route::delete('grade/{id}', 'GradeController@destroy');
 Route::delete('company/{id}', 'CompanyController@destroy');
 Route::delete('student/{id}', 'StudentController@destroy');
 
+Route::post('editpetition/{id}', 'PetitionController@update');
 Route::post('editgrade/{id}', 'GradeController@update');
 Route::post('editcompany/{id}', 'CompanyController@update');
 Route::post('editstudent/{id}', 'StudentController@update');
